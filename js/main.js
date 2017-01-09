9ebfaf2a69baf2c21fae115eea63ee40570cd08f
inson <mtzafar05@yahoo.com>
2017-01-09 16:51:31 -0500

    getting json data

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
-var app=angular.module('app', []);
-app.service('ApiService', function(){
+  var app=angular.module('app', []);
+  app.service('ApiService', function($http){
 
-	this.loadData=function(){}
-});
+    this.getData=function(link){
+      return $http({
+        method: 'GET',
+        url: link
+      });
+    }
+  });
 
-app.controller('DataController', function($scope, $rootScope, ApiService){
+  app.controller('DataController', function($scope, $rootScope, ApiService){
 
+    $rootScope.text='root text';
+    $scope.name='DataController';
+    $scope.load=function(){
+      alert('button clicked');
+    }
 
-	$scope.loadData=function(){
-		alert('button clicked');
-	}
+    $scope.dataset=ApiService.getData('https://data.baltimorecity.gov/resource/m8g9-abgb.json').then(function(res){
+      console.dir(res.data);
 
-$scope.dataset=ApiService.loadData();
+    }, function(err){
+      console.dir(err.message);
 
-});
+    });
+
+  }).controller('TestController', function($scope, $rootScope){
+    $scope.name='TestController';
+    $scope.testing=function(){
+      alert('test function');
+    }
+  });
